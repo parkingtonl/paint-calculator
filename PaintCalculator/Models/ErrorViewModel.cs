d31aca960c167d3f633395a3e18f1eaeccd324ba
Lance Parkington <lance.parkington@dyballassociates.co.uk>
2020-03-03 17:37:42 +0000

    Add project files.

---
+++ PaintCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PaintCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
