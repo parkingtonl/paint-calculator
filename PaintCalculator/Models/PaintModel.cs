467a5053b3bc75fec3444e19515e41d77af39393
Lance Parkington <lance.parkington@dyballassociates.co.uk>
2020-03-04 20:52:31 +0000

    Trouble - No Database need to validata client side - run out of time

---
+++ PaintCalculator/Models/PaintModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaintCalculator.Models
+{
+    public class PaintModel
+    {
+        [Required]
+        public string Length { get; set; }
+
+        public string Width { get; set; }
+
+        public string Height { get; set;}
+    }
+}
